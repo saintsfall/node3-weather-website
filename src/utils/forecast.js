8f4747d2f03a4799f76f764965fccbdad624447d
Caio Santos <caio.f.santos@icloud.com>
2019-06-19 13:40:20 -0300

    Added more info in the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -20,6 +20,7 @@ const forecast = (latitude, longitude, callback) => {
 ${response.daily.data[0].summary}
 It is currently ${response.currently.temperature} degrees out.
 There is a ${response.currently.precipProbability}% chance of rain.
+There is a min of ${response.daily.data[0].temperatureMin} and a max of ${response.daily.data[0].temperatureMax}
         `
         );
       }
